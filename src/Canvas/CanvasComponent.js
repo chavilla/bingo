9b3552b9e0c970a3fbd6a1455f5bc6cc4d3ca7b6
chavilla <jcharris.villa@gmail.com>
2021-12-08 17:32:48 -0500

    ajustes

--- src/Canvas/CanvasComponent.js
+++ src/Canvas/CanvasComponent.js
@@ -4,7 +4,8 @@ import { bible_books } from "../utils/helpers";
 import BookBox from "./BookBox";
 
 
-let books_storaged = [];
+let books_storaged_state = [];
+let books_bible_pending = bible_books;
 
 const CanvasComponent = () => {
   const [finished, setFinished] = useState(false);
@@ -12,9 +13,11 @@ const CanvasComponent = () => {
   
   useEffect(() => {
     const get_books_storaged = JSON.parse(localStorage.getItem("books")) || null;
+    const books_storaged_pending = JSON.parse(localStorage.getItem("books_bible_storaged")) || null;
     if (get_books_storaged) {
       setBooksCanvas(get_books_storaged);
-      books_storaged = get_books_storaged;
+      books_storaged_state = get_books_storaged;
+      books_bible_pending = books_storaged_pending;
     }
   },[])
 
@@ -25,18 +28,19 @@ const CanvasComponent = () => {
     const random_code = Math.floor(Math.random() * (max - min)) + min;
 
     // 02. Select a book from the array
-    const selected_book = bible_books[random_code];
+    const selected_book = books_bible_pending[random_code];
 
     // 03. delete the element from the bible books array
-    bible_books.splice(random_code, 1);
+    books_bible_pending.splice(random_code, 1);
 
     //04. set the book selected in a row to render
     setBooksCanvas([...booksCanvas, selected_book]);
-    books_storaged.push(selected_book);
-    localStorage.setItem("books", JSON.stringify(books_storaged));
+    books_storaged_state.push(selected_book);
+    localStorage.setItem("books", JSON.stringify(books_storaged_state));
+    localStorage.setItem("books_bible_storaged", JSON.stringify(books_bible_pending));
 
     // 05. conditional to finish the game
-    if (bible_books.length === 0) {
+    if (books_bible_pending.length === 0) {
       setFinished(true);
     }
   };
@@ -49,14 +53,14 @@ const CanvasComponent = () => {
           type="button"
           className="btn_shake"
           disabled={finished}
-          onClick={() => getRandomArbitrary(0, bible_books.length)}
+          onClick={() => getRandomArbitrary(0, books_bible_pending.length)}
         >
           Elegir libro
         </button>
       </div>
       {finished && <p className="text-center">Juego terminado</p>}
       <div className="books_list_box">
-        <h2 className="text-center">Libros generados</h2>
+        <h2 className="text-center">Libros generados { books_storaged_state.length}/66 </h2>
         <div className="books_list">
           {booksCanvas.map((book, index) => (
             <BookBox key={ index } book={book} />
