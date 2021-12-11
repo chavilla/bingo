aa78a625020098dc0ef553f62b66e3b44859220d
chavilla <jcharris.villa@gmail.com>
2021-12-11 14:50:39 -0500

    ajustes

--- src/Canvas/CanvasComponent.js
+++ src/Canvas/CanvasComponent.js
@@ -7,9 +7,47 @@ let books_storaged_state = [];
 let books_bible_pending = bible_books;
 
 const CanvasComponent = () => {
+  const [finished, setFinished] = useState(false);
+  const [booksCanvas, setBooksCanvas] = useState([]);
 
+  useEffect(() => {
+    const get_books_storaged =
+      JSON.parse(localStorage.getItem("books")) || null;
+    const books_storaged_pending =
+      JSON.parse(localStorage.getItem("books_bible_storaged")) || null;
+    if (get_books_storaged) {
+      setBooksCanvas(get_books_storaged);
+      books_storaged_state = get_books_storaged;
+      books_bible_pending = books_storaged_pending;
+    }
+  }, []);
 
-  /* const getRandomArbitrary = (min, max) => {
+  const showSweetAlert = (min, max) => {
+    let timerInterval;
+    Swal.fire({
+      title: "Elegir Libro",
+      html: "Seleccionando en <b></b> milisegundos.",
+      timer: 2000,
+      timerProgressBar: true,
+      didOpen: () => {
+        Swal.showLoading();
+        const b = Swal.getHtmlContainer().querySelector("b");
+        timerInterval = setInterval(() => {
+          b.textContent = Swal.getTimerLeft();
+        }, 100);
+      },
+      willClose: () => {
+        clearInterval(timerInterval);
+      },
+    }).then((result) => {
+      /* Read more about handling dismissals below */
+      if (result.dismiss === Swal.DismissReason.timer) {
+        getRandomArbitrary(min,max)
+      }
+    });
+  };
+
+  const getRandomArbitrary = (min, max) => {
     // 01. Generating a code
     const random_code = Math.floor(Math.random() * (max - min)) + min;
 
@@ -35,11 +73,31 @@ const CanvasComponent = () => {
 
     Swal.fire(`Libro seleccionado: \n ${selected_book}`);
 
-  }; */
+  };
 
   return (
     <div className="canvas_component_box">
-      
+      <div className="btn_shake_box">
+        <button
+          type="button"
+          className="btn_shake"
+          disabled={finished}
+          onClick={() => showSweetAlert(0, books_bible_pending.length) }
+        >
+          Elegir libro
+        </button>
+      </div>
+      {finished && <p className="text-center">Juego terminado</p>}
+      <div className="books_list_box">
+        <h2 className="text-center">
+          Libros generados {booksCanvas.length}/66{" "}
+        </h2>
+        <div className="books_list">
+          {booksCanvas.map((book, index) => (
+            <BookBox key={index} book={book} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
